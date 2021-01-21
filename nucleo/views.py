100202507264ede78baa2001266a1e2114c92bcc
SanchezAdrian <49305131+SanchezAdrian@users.noreply.github.com>
2021-01-21 10:32:20 +0100

    21 01

--- nucleo/views.py
+++ nucleo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from nucleo.models import *
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def ListClientes(request):
+    listaClientes = Cliente.objects.all()
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nucleo',
+    
     
 ]
 
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL='index/'
+
+LOGOUT_REDIRECT_URL='/'
