2fab643f41a6175390a80dfc306d1540a3eb853e
SanchezAdrian <49305131+SanchezAdrian@users.noreply.github.com>
2021-02-17 19:29:03 +0100

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -97,9 +97,10 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
