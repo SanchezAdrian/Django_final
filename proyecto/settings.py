608017ad9d3a035d60d6cc78eb32e23a403c8a2d
SanchezAdrian <49305131+SanchezAdrian@users.noreply.github.com>
2021-02-17 15:21:55 +0100

    Update settings.py

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -189,6 +189,4 @@ REST_FRAMEWORK = {
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+
